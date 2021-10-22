c49812660ded28ab2c7587fcb375412a4d5d680f
msolorio <msolorio.github@gmail.com>
2021-10-21 18:18:01 -0700

    Adds API calls and resolvers for retrieving a single Track

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,12 +3,20 @@ const resolvers = {
     // returns tracks to populate homepage
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
+    },
+
+    // returns a track by id for track page
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);
     }
   },
 
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
+    },
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
     }
   }
 };
--- server/src/schema.js
+++ server/src/schema.js
@@ -4,22 +4,34 @@ const typeDefs = gql`
   type Query {
     "Tracks data for the homepage"
     tracksForHome: [Track!]!
+    track(id: ID!): Track
   }
 
   type Track {
     id: ID!
     title: String!
     author: Author!
-    "url of primary image of the track"
+    "Url of primary image of the track"
     thumbnail: String
     length: Int
     modulesCount: Int
+    description: String
+    "Number of times track has been viewed"
+    numberOfViews: Int
+    modules: [Module!]!
   }
 
   type Author {
     id: ID!
     name: String!
     photo: String
+  }
+
+  type Module {
+    id: ID!
+    title: String!
+    "Modules length in minutes"
+    length: Int
   }
 `;
 
